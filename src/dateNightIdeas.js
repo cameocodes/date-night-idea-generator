73a6185fccb83f28f327a7f4cbe0ab5f176d9c7c
Cameo Langford <hello@cameocodes.com>
2020-03-08 12:19:53 +1100

    Added date night ideas, ready to deploy

--- src/dateNightIdeas.js
+++ src/dateNightIdeas.js
@@ -1,25 +1,280 @@
 const ideas = [
     {
         "id": 1,
-        "idea": "blah"
+        "idea": "go out for a picnic!"
     },{
         "id": 2,
-        "idea": "erghthtfsdf"
+        "idea": "go for a bushwalk!"
     },{
         "id": 3,
-        "idea": "utyrfvx"
+        "idea": "go visit a winery or brewery!"
     },{
         "id": 4,
-        "idea": "mdgklfskdogn"
+        "idea": "go camping!"
     },{
         "id": 5,
-        "idea": "fiosdfnlkf"
+        "idea": "go on a road trip!"
     },{
         "id": 6,
-        "idea": "dlfksgnlksdf"
+        "idea": "go to an outdoor cinema!"
     },{
         "id": 7,
-        "idea": "lkgndfsglsdk"
+        "idea": "take a pottery or art class!"
+    },{
+        "id": 8,
+        "idea": "go to an art gallery!"
+    },{
+        "id": 9,
+        "idea": "go to the museum!"
+    },{
+        "id": 10,
+        "idea": "go to a dinner in the dark event!"
+    },{
+        "id": 11,
+        "idea": "go to an observatory!"
+    },{
+        "id": 12,
+        "idea": "do karaoke!"
+    },{
+        "id": 13,
+        "idea": "make fresh homemade pizzas!"
+    },{
+        "id": 14,
+        "idea": "go to an amusement park!"
+    },{
+        "id": 15,
+        "idea": "go out for cocktails!"
+    },{
+        "id": 16,
+        "idea": "go on a pub crawl!"
+    },{
+        "id": 17,
+        "idea": "go home and bake things!"
+    },{
+        "id": 18,
+        "idea": "have a games night!"
+    },{
+        "id": 19,
+        "idea": "have a movie night!"
+    },{
+        "id": 20,
+        "idea": "go to the zoo!"
+    },{
+        "id": 21,
+        "idea": "go to the aquarium!"
+    },{
+        "id": 22,
+        "idea": "drive to the beach!"
+    },{
+        "id": 23,
+        "idea": "go to a concert!"
+    },{
+        "id": 24,
+        "idea": "go to a comedy show!"
+    },{
+        "id": 25,
+        "idea": "do something neither of us have done before!"
+    },{
+        "id": 26,
+        "idea": "go to a park and play sports!"
+    },{
+        "id": 27,
+        "idea": "go play pool!"
+    },{
+        "id": 28,
+        "idea": "make cocktails at home!"
+    },{
+        "id": 29,
+        "idea": "go to a sports game!"
+    },{
+        "id": 30,
+        "idea": "do indoor trampolining!"
+    },{
+        "id": 31,
+        "idea": "go to a night market!"
+    },{
+        "id": 32,
+        "idea": "go skinny dipping!"
+    },{
+        "id": 33,
+        "idea": "go explore a neighbourhood!"
+    },{
+        "id": 34,
+        "idea": "go geocaching!"
+    },{
+        "id": 35,
+        "idea": "go rollerskating!"
+    },{
+        "id": 36,
+        "idea": "find a movie screening in a park!"
+    },{
+        "id": 37,
+        "idea": "take a free class in something we've never done before!"
+    },{
+        "id": 38,
+        "idea": "do a scavenger hunt at the museum!"
+    },{
+        "id": 39,
+        "idea": "have a sandcastle building contest!"
+    },{
+        "id": 40,
+        "idea": "go play bingo!"
+    },{
+        "id": 41,
+        "idea": "make a kite and try to fly it!"
+    },{
+        "id": 42,
+        "idea": "build a blanket fort and watch a movie!"
+    },{
+        "id": 43,
+        "idea": "go ghost hunting!"
+    },{
+        "id": 44,
+        "idea": "learn to give massages!"
+    },{
+        "id": 45,
+        "idea": "make a time capsule!"
+    },{
+        "id": 46,
+        "idea": "take a laptop somewhere and have an ourdoor movie screening!"
+    },{
+        "id": 47,
+        "idea": "follow YouTube videos to try and cook new weird foods!"
+    },{
+        "id": 48,
+        "idea": "create a new holiday with our own rules and traditions to celebrate every year!"
+    },{
+        "id": 49,
+        "idea": "play a horror film drinking game!"
+    },{
+        "id": 50,
+        "idea": "learn some magic tricks!"
+    },{
+        "id": 51,
+        "idea": "pretend to be tourists!"
+    },{
+        "id": 52,
+        "idea": "go to the Queen Victoria Market!"
+    },{
+        "id": 53,
+        "idea": "go to a local art show!"
+    },{
+        "id": 54,
+        "idea": "go to an open mic night!"
+    },{
+        "id": 55,
+        "idea": "go stargazing!"
+    },{
+        "id": 56,
+        "idea": "go to a playground!"
+    },{
+        "id": 57,
+        "idea": "go horseriding!"
+    },{
+        "id": 58,
+        "idea": "go on a treetop walk!"
+    },{
+        "id": 59,
+        "idea": "go mountain-biking!"
+    },{
+        "id": 60,
+        "idea": "create a scavenger hunt!"
+    },{
+        "id": 61,
+        "idea": "get creative together!"
+    },{
+        "id": 62,
+        "idea": "take a yoga class together!"
+    },{
+        "id": 63,
+        "idea": "Play hide and seek somewhere!"
+    },{
+        "id": 64,
+        "idea": "have an at-home spa night!"
+    },{
+        "id": 65,
+        "idea": "go bowling!"
+    },{
+        "id": 66,
+        "idea": "go play lawn bowls!"
+    },{
+        "id": 67,
+        "idea": "go for an ice-cream date!"
+    },{
+        "id": 68,
+        "idea": "go see a local sports event!"
+    },{
+        "id": 69,
+        "idea": "go to a meditation class!"
+    },{
+        "id": 70,
+        "idea": "make our own life lists!"
+    },{
+        "id": 71,
+        "idea": "find an obscure Guiness World Record and try to break it!"
+    },{
+        "id": 72,
+        "idea": "go play arcade games!"
+    },{
+        "id": 73,
+        "idea": "go to a golfing range!"
+    },{
+        "id": 74,
+        "idea": "create a couple's bucket list!"
+    },{
+        "id": 75,
+        "idea": "go to a bookstore, find travel books and plan out a dream trip!"
+    },{
+        "id": 76,
+        "idea": "go to an op-shop and pick outfits for each other to try on!"
+    },{
+        "id": 77,
+        "idea": "play video games together!"
+    },{
+        "id": 78,
+        "idea": "recreate our first date!"
+    },{
+        "id": 79,
+        "idea": "go out to a fancy resturant for dinner!"
+    },{
+        "id": 80,
+        "idea": "have a wine and cheese night!"
+    },{
+        "id": 81,
+        "idea": "go for a mini road-trip!"
+    },{
+        "id": 82,
+        "idea": "go to a nursery and pick out a new plant together!"
+    },{
+        "id": 83,
+        "idea": "have a paper plane contest!"
+    },{
+        "id": 84,
+        "idea": "visit an historical landmark!"
+    },{
+        "id": 85,
+        "idea": "make homemade ice-cream sundaes!"
+    },{
+        "id": 86,
+        "idea": "go to a jazz club!"
+    },{
+        "id": 87,
+        "idea": "go rockclimbing or bouldering!"
+    },{
+        "id": 88,
+        "idea": "buy takeaway Chinese food and have a candlelit dinner!"
+    },{
+        "id": 89,
+        "idea": "have a fondue night!"
+    },{
+        "id": 90,
+        "idea": "have an indoor picnic!"
+    },{
+        "id": 91,
+        "idea": "make breakfast for dinner!"
+    },{
+        "id": 92,
+        "idea": "pick a movie or TV show and play a drinking game!"
     }
 ]
 
